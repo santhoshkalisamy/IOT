d411732e67aacd6a59bf87dc7ac23eb56e6a1011
Santhosh K <53000229+santhoshkalisamy@users.noreply.github.com>
2020-08-25 23:16:58 +0530

    Added temperature code

--- Welcome.ino
+++ Welcome.ino
@@ -30,15 +30,57 @@
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+const int sensor=A1;
+float tempc;  
+float tempf;  
+float vout; 
+
 void setup() {
+  Serial.begin(9600);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
 
   showWelcomeMessage();  
 
+  pinMode(sensor,INPUT);
+
 }
 
 void loop() {
+vout=analogRead(sensor);
+Serial.print("Analog Value = ");
+Serial.print(vout);
+Serial.println();
+vout=(vout*5000)/1023;
+tempc=vout/10; // Storing value in Degree Celsius
+tempf=(tempc*1.8)+32; // Converting to Fahrenheit 
+Serial.print("in DegreeC=");
+Serial.print("\t");
+Serial.print(tempc);
+Serial.println();
+Serial.print("in Fahrenheit=");
+Serial.print("\t");
+Serial.print(tempf);
+Serial.println();
+showTemp(tempf, tempc);
+delay(1000); //Delay of 1 second for ease of viewing 
+}
+
+void showTemp(int tempf, int tempc) {
+
+  display.clearDisplay();
+  display.setTextSize(2); 
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(40, 0);
+  display.println(F("Temp"));
+  display.setCursor(0, 20);
+  display.print(F("F: "));
+  display.println(tempf);
+  display.setCursor(0, 40);
+  display.print(F("C: "));
+  display.println(tempc);
+
+  display.display();    // Show initial text
 }
 
 void showWelcomeMessage(void) {
